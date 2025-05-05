eeecc80bd2a3275f1b5d87d2b895df4d71611045
Kamil Akhmedkhanov <=>
2025-05-05 18:31:41 +0300

    fix dto

--- src/flowers/dto/create-flower.dto.ts
+++ src/flowers/dto/create-flower.dto.ts
@@ -1,9 +1,18 @@
-import { IsNotEmpty, IsNumber, IsString, IsArray } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsArray,
+  IsOptional,
+} from 'class-validator';
 
 export class CreateFlowerDto {
   @IsNotEmpty()
   @IsString()
   name: string;
+
+  @IsOptional()
+  description: string;
 
   @IsNotEmpty()
   @IsNumber()
