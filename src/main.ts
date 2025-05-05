77029a7dff3f6af3c96b4a379be162b09d422aec
Kamil Akhmedkhanov <=>
2025-05-05 18:08:35 +0300

    add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // удаляет неописанные поля из DTO
+      transform: true, // преобразует payload в нужный тип
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
