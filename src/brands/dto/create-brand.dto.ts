6487a5e4f4eacd32e129c1eabc069bd5b43e01b6
Kamil Akhmedkhanov <=>
2025-05-06 16:17:26 +0300

    change brand entity

--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,7 +1,26 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateBrandDto {
   @IsNotEmpty()
   @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  slug: string;
+
+  @IsEmail()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  sitePhone: string;
+
+  @IsNotEmpty()
+  schedule: {
+    from: string;
+    to: string;
+  };
 }
--- src/brands/entities/brand.entity.ts
+++ src/brands/entities/brand.entity.ts
@@ -7,6 +7,18 @@ export class Brand extends AbstractEntity {
   @Column()
   name: string;
 
+  @Column({ unique: true })
+  slug: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ type: 'json' })
+  schedule: { from: string; to: string };
+
+  @Column({ unique: true })
+  sitePhone: string;
+
   @OneToMany(() => Flower, (flower) => flower.brand)
   flowers: Flower[];
 }
