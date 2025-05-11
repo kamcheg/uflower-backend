064af6566226f8fd797a9fb845aa7450bdb7dd90
Kamil Akhmedkhanov <=>
2025-05-11 22:43:13 +0300

    fix

--- src/seed/main.ts
+++ src/seed/main.ts
@@ -1,6 +1,7 @@
 // TODO
 import { DataSource } from 'typeorm';
 import { Size } from '../modules/sizes/entities/size.entity';
+import { FlowerType } from '../modules/flower-types/entities/flower-type.entity';
 
 const AppDataSource = new DataSource({
   type: 'mysql',
@@ -15,17 +16,70 @@ const AppDataSource = new DataSource({
 
 async function seed() {
   await AppDataSource.initialize();
-  const repo = AppDataSource.getRepository(Size);
 
-  await repo.save([
-    {
-      title: 'Большой',
-      image:
-        'https://uflor.ru/upload/uf/04a/bzz1i3vkvec0pwelj6p0m9wloz1n2rt3.svg',
-    },
-  ]);
+  await initSizes();
+  await initFlowerTypes();
 
   await AppDataSource.destroy();
 }
 
 seed();
+
+async function initSizes() {
+  const repo = AppDataSource.getRepository(Size);
+
+  await repo.save([
+    {
+      title: 'Стандарт',
+      image:
+        'https://uflor.ru/upload/uf/99d/7r1i43d0ct21y03e81bfkl3u6bhali1j.svg',
+    },
+    {
+      title: 'Стандарт +',
+      image:
+        'https://uflor.ru/upload/uf/b7f/592vaa3hmhmdx42havleol3a484d46ps.svg',
+    },
+    {
+      title: 'Большой',
+      image:
+        'https://uflor.ru/upload/uf/04a/bzz1i3vkvec0pwelj6p0m9wloz1n2rt3.svg',
+    },
+    {
+      title: 'Огромный',
+      image:
+        'https://uflor.ru/upload/uf/442/p0uxdb675op0xnac0jdv9mf0zcgzimik.svg',
+    },
+  ]);
+}
+
+async function initFlowerTypes() {
+  const repo = AppDataSource.getRepository(FlowerType);
+
+  await repo.save([
+    { title: 'Розы' },
+    { title: 'Гипсофила' },
+    { title: 'Ранункулюс' },
+    { title: 'Роза кустовая' },
+    { title: 'Ирис' },
+    { title: 'Сирень' },
+    { title: 'Роза пионовидная' },
+    { title: 'Лилии' },
+    { title: 'Тюльпаны' },
+    { title: 'Альстромерия' },
+    { title: 'Лизиантусы' },
+    { title: 'Тюльпаны пионовидные' },
+    { title: 'Анемоны' },
+    { title: 'Ландыши' },
+    { title: 'Фрезия' },
+    { title: 'Гиацинт' },
+    { title: 'Мимозы' },
+    { title: 'Хризантемы' },
+    { title: 'Герберы' },
+    { title: 'Орхидея' },
+    { title: 'Эустома' },
+    { title: 'Гвоздики' },
+    { title: 'Подсолнух' },
+    { title: 'Гортензия' },
+    { title: 'Ромашки' },
+  ]);
+}
