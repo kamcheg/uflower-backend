37ced6759234957584f5c7596631c387cc6c5ff9
Kamil Akhmedkhanov <=>
2025-05-13 11:47:21 +0300

    fix seeds env

--- src/seed/main.ts
+++ src/seed/main.ts
@@ -1,4 +1,5 @@
 // TODO
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 import { Size } from '../modules/sizes/entities/size.entity';
 import { FlowerType } from '../modules/flower-types/entities/flower-type.entity';
@@ -11,11 +12,11 @@ import { User } from '../modules/users/entities/user.entity';
 
 const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'fl-store',
+  host: process.env.DATABASE_HOST,
+  port: +(process.env.DATABASE_PORT || 3306),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 });
