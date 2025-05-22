2dd9f431f5c0219f4fd1ea8491eb8a6d98e0cf97
Kamil Akhmedkhanov <=>
2025-05-22 15:54:53 +0300

    fix

--- src/common/domain.decorator.ts
+++ src/common/domain.decorator.ts
@@ -6,6 +6,10 @@ export const Domain = createParamDecorator(
     const request = ctx.switchToHttp().getRequest<Request>();
     const host = request.headers.host;
 
+    if (request) {
+      return 'magas-flowers.ru';
+    }
+
     if (typeof host === 'string') {
       return host.split(':')[0]; // убираем порт, если есть
     }
