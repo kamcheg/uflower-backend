d31cc0d98753b7410b231b49ac0b3130e51fa771
Kamil Akhmedkhanov <=>
2025-05-22 15:49:55 +0300

    fix

--- src/common/domain.decorator.ts
+++ src/common/domain.decorator.ts
@@ -1,26 +1,15 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
-// TODO
 export const Domain = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string => {
     const request = ctx.switchToHttp().getRequest<Request>();
-    const origin = request.headers.origin;
-    const referer = request.headers.referer;
     const host = request.headers.host;
 
-    try {
-      if (typeof origin === 'string') {
-        return new URL(origin).hostname;
-      } else if (typeof referer === 'string') {
-        return new URL(referer).hostname;
-      } else if (typeof host === 'string') {
-        return host.split(':')[0]; // убрать порт
-      } else {
-        return 'magas-flowers.ru'; // TODO
-      }
-    } catch {
-      return 'magas-flowers.ru'; // TODO
+    if (typeof host === 'string') {
+      return host.split(':')[0]; // убираем порт, если есть
     }
+
+    return 'none';
   },
 );
