7d78b2d58b2bb78a0792b6206f3e81a173d5e1b3
Kamil Akhmedkhanov <=>
2025-05-22 15:35:42 +0300

    fix

--- src/common/domain.decorator.ts
+++ src/common/domain.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
 
+// T
 export const Domain = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string => {
     const request = ctx.switchToHttp().getRequest<Request>();
