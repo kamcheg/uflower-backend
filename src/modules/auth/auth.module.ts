1b091e1a843aeb108ae2ad2eef284795f5093d86
Kamil Akhmedkhanov <=>
2025-06-06 00:05:57 +0300

    fix

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,21 +1,18 @@
-import * as dotenv from 'dotenv';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 
-dotenv.config();
-const secret = process.env.JWT_SECRET;
-const expiresIn = process.env.JWT_EXPIRES;
-
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
       global: true,
-      secret,
-      signOptions: { expiresIn },
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: process.env.JWT_EXPIRES ?? '1d' },
+      }),
     }),
   ],
   controllers: [AuthController],
